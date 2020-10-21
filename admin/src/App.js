15a9af46f50e402884727870b287833fe3569a67
Molnár Antal <molnar.antal@pharmapromo.hu>
2020-10-21 15:57:20 +0200

    Modify App.js to point at react-admin

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Admin } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const dataProvider = jsonServerProvider('http://localhost:3004'); // todo: move this to a config
+
+const App = () => {
+    return <Admin dataProvider={dataProvider}>{/* TODO */}</Admin>;
+};
 
 export default App;
