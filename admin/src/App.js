75362581f5b1fd00319e3f8d23eedfa4082044f3
Tóth Csaba <tocsamanatee@gmail.com>
2020-10-23 17:57:29 +0200

    Added edit to the users Resource. Closes #71

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import UserList from './components/UserList'
+import UserEdit from './components/UserEdit'
 
 const dataProvider = jsonServerProvider('http://localhost:3004'); // todo: move this to a config
 
 const App = () => {
     return <Admin dataProvider={dataProvider}>
-        <Resource name='users' list={UserList} />
+        <Resource name='users' list={UserList} edit={UserEdit} />
     </Admin>;
 };
 
