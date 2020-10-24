0d54ed3496876e0f20a77e5420c6afe6473484ed
Czinke Márton <czinke.marci@gmail.com>
2020-10-24 22:34:07 +0200

    Finish the create function component

--- admin/src/components/CarCreate.js
+++ admin/src/components/CarCreate.js
@@ -1,21 +1,77 @@
-import React, {useRef}from 'react';
+import * as React from "react";
+import {
+  Create,
+  Edit,
+  SimpleForm,
+  TextInput,
+  DateInput,
+  ReferenceManyField,
+  Datagrid,
+  TextField,
+  DateField,
+  EditButton,
+} from "react-admin";
 
-export const CreateCar = () => {
-    const carManufacturer= useRef(); //Márka
-    const carModel = useRef(); //Modell
-    const carManufacturingDate = useRef(); //Gyártási év
-    const carPlateNumber = useRef(); //Rendszám
+const helpManufacturer = "The name of the car manufacturer";
+const helpModel = "The name of the car model";
+const helpManufacturingDate = "Pick the car Manufacturing Date";
+const helpPlate = "The license plate number";
 
-    const Adding = () => {
+const labelManufacturer = "Manufacturer";
+const labelModel = "Model";
+const labelManufacturingDate = "Manufacturing Date";
+const labelPlate = "License Plate Number";
 
-    }
+const carManufacturer;
+const carManufacturingDate;
+const carModel;
+const carPlate;
+const carDateCreated;
+const carDateUpdated;
+const carDateDeleted;
 
-    return <form className="CarCreateForm">
-        <input type="text" placeholder="Manufacturer" id="manufacturer" ref={carManufacturer}/>
-        <input type="text" placeholder="Model" id="model" ref={carModel}/>
-        <input type="number" placeholder="Manufacturing date" id="date" ref/>
-        <input type="text" placeholder="Plate" id="plate"/>
-        
-        <button onClick={Adding}></button>
-    </form>
-}
+export const CarCreate = (props) => (
+  <Create {...props}>
+    <SimpleForm>
+      <TextInput
+        source="manufacturer"
+        label={labelManufacturer}
+        helperText={helpManufacturer}
+      />
+      <TextInput source="model" label={labelModel} helperText={helpModel} />
+      <DateInput
+        source="manufacturing_date"
+        label={labelManufacturingDate}
+        helperText={helpManufacturingDate}
+      />
+      <TextInput
+        source="license_plate"
+        label={labelPlate}
+        helperText={helpPlate}
+      />
+    </SimpleForm>
+  </Create>
+);
+
+export const CarEdit = (props) => (
+  <Edit {...props}>
+    <SimpleForm>
+      <TextInput disabled label="Id" source="id" />
+      <TextInput source="title" />
+      <TextInput multiline source="teaser" />
+      <TextInput source="body" />
+      <DateInput label="Publication date" source="published_at" />
+      <ReferenceManyField
+        label="Comments"
+        reference="comments"
+        target="post_id"
+      >
+        <Datagrid>
+          <TextField source="body" />
+          <DateField source="created_at" />
+          <EditButton />
+        </Datagrid>
+      </ReferenceManyField>
+    </SimpleForm>
+  </Edit>
+);
