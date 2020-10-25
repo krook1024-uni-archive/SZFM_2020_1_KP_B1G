df23348627cc7e039cc22b4b2e12299aad376cc1
fulekylaszlo <fulekylaszlo@gmail.com>
2020-10-25 19:09:41 +0100

    Created "CarList component."

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import { Admin, Resource } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
-import UserList from './components/UserList'
-import UserEdit from './components/UserEdit'
-import RentList from './components/RentList'
+import React from "react";
+import { Admin, Resource } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
+import UserList from "./components/UserList";
+import UserEdit from "./components/UserEdit";
+import RentList from "./components/RentList";
+import CarList from "./components/CarList";
 
-const dataProvider = jsonServerProvider('http://localhost:3004'); // todo: move this to a config
+const dataProvider = jsonServerProvider("http://localhost:3004"); // todo: move this to a config
 
 const App = () => {
-    return <Admin dataProvider={dataProvider}>
-        <Resource name='users' list={UserList} edit={UserEdit} />
-        <Resource name='rents' list={RentList} />
-    </Admin>;
+  return (
+    <Admin dataProvider={dataProvider}>
+      <Resource name="users" list={UserList} edit={UserEdit} />
+      <Resource name="rents" list={RentList} />
+      <Resource name="cars" list={CarList} />
+    </Admin>
+  );
 };
 
 export default App;
