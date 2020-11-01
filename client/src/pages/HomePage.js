423766f5d1f9824b273f72836b5154c18bdbe119
Molnár Antal <krook1024@gmail.com>
2020-11-01 16:19:17 +0100

    Wrote a scratch for the CarList

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,12 +1,23 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { useEffect } from "react";
+import { Container, Spinner } from "react-bootstrap";
+import CarList from "../components/client/CarList";
+import { useRentalState, useRentalDispatch } from "../context/rental-context";
 
 export default function HomePage() {
+  const { cars, cars_loading } = useRentalState();
+  const dispatch = useRentalDispatch();
+
+  useEffect(() => {
+    dispatch({ type: "get_cars" });
+  }, [dispatch]);
+
   return (
     <Container>
       <div>
         <h1>Hi!</h1>
         <p>I'm the homepage component.</p>
+        {cars_loading && <Spinner animation='grow' />}
+        {!cars_loading && <CarList cars={cars}>asdasd</CarList>}
       </div>
     </Container>
   );
