44b56cc7a3371fbf47f899255da4a166edeef4b8
Molnár Antal <krook1024@gmail.com>
2020-11-10 19:22:46 +0100

    Move data fetching from context to HomePage component

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,27 +3,34 @@ import { Container, Spinner } from "react-bootstrap";
 import CarList from "../components/client/CarList";
 import { useRentalState, useRentalDispatch } from "../context/rental-context";
 import Login from "../components/client/Login";
-
+import axios from "axios";
 
 export default function HomePage() {
   const { cars, cars_loading } = useRentalState();
   const dispatch = useRentalDispatch();
 
   useEffect(() => {
-    dispatch({ type: "get_cars" });
+    const url = "http://localhost:3004/cars";
+    axios
+      .get(url)
+      .then((response) => {
+        dispatch({ type: "set_cars", payload: response.data });
+      })
+      .catch((err) => console.log(err));
   }, [dispatch]);
 
   return (
-    <Container>
-      <div>
-        <Login></Login>
-      </div>
-      <div>
-        <h1>Hi!</h1>
-        <p>I'm the homepage component.</p>
-        {cars_loading && <Spinner animation='grow' />}
-        {!cars_loading && <CarList cars={cars}>asdasd</CarList>}
-      </div>
-    </Container>
+    <>
+      {/* ide kellene majd a navbar*/}
+      <Container>
+        <div>
+          <Login />
+        </div>
+        <div>
+          {cars_loading && <Spinner animation="grow" />}
+          {!cars_loading && <CarList cars={cars} />}
+        </div>
+      </Container>
+    </>
   );
 }
