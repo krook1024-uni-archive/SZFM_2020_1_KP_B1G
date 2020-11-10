0b290267c06170f14151111202b637c31a3d9303
Molnár Antal <krook1024@gmail.com>
2020-11-10 19:23:17 +0100

    Changed Car component a bit, implemented CarList

--- client/src/components/client/CarList.js
+++ client/src/components/client/CarList.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { CardDeck } from "react-bootstrap";
+import Car from "../client/Car";
 
 const CarList = ({ cars }) => {
-  return <div>Im the car list component</div>;
+  if (Array.isArray(cars)) {
+    return (
+      <>
+        <CardDeck>
+          {cars.map((car) => (
+            <Car car={car} />
+          ))}
+        </CardDeck>
+      </>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default CarList;
