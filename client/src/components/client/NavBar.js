f9fc1d942fa8121a85aa2654066985f78bdcdd05
fulekylaszlo <fulekylaszlo@gmail.com>
2020-11-13 20:53:02 +0100

    Fixed navbar width.

--- client/src/components/client/NavBar.js
+++ client/src/components/client/NavBar.js
@@ -5,20 +5,18 @@ import Login from "../client/Login";
 class navBar extends React.Component {
   render() {
     return (
-      <Container>
-        <Navbar bg="dark" variant="dark">
-          <Navbar.Brand href="#home">Cégünk neve</Navbar.Brand>
-          <Nav pullRight></Nav>
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="ml-auto">
-              <Button href="/">Car</Button>{" "}
-              <div>
-                <Login />
-              </div>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </Container>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Cégünk neve</Navbar.Brand>
+        <Nav pullRight></Nav>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <Button href="/">Car</Button>{" "}
+            <div>
+              <Login />
+            </div>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
