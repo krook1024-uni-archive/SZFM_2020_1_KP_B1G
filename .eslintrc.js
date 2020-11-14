e5a3aa26a0d9a7239edd21a6b5c14a286faf6c4f
Molnár Antal <krook1024@gmail.com>
2020-11-14 20:05:15 +0100

    Linter for the server

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+        "browser": false,
+        "es2021": false,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    "rules": {
+    }
+};
