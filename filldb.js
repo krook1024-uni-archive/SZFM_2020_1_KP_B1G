34c653b0aa4f3ef0d029cd4dbe1030d5abaa7b49
Molnár Antal <krook1024@gmail.com>
2020-11-14 13:10:40 +0100

    Created a script to fill the database with sample data

---
+++ filldb.js
@@ -0,0 +1,66 @@
+// fills the MongoDB database with random data using Faker.js
+// USAGE: node filldb.js
+
+const mongoose = require("mongoose");
+const faker = require("faker");
+
+require("dotenv").config();
+
+const mongo_config = {
+    auth: { authSource: "admin" },
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+};
+
+mongoose.connect(process.env.MONGO_URI, mongo_config);
+var db = mongoose.connection;
+
+db.once("open", () => {
+    const UserModel = require("./models/User");
+    const CarModel = require("./models/Car");
+
+    var users = [];
+    for (var i = 0; i < 50; i++) {
+        users = [
+            ...users,
+            {
+                name: faker.name.findName(),
+                email: faker.internet.email(),
+                password: faker.internet.password(),
+                dateOfBirth: faker.date.past(),
+                licenseCategory: "B",
+                createdAt: faker.date.past(),
+            },
+        ];
+    }
+
+    UserModel.insertMany(users, (error, docs) => console.log(error, docs));
+
+    var cars = [];
+    var seatNumbers = [2, 3, 5, 7];
+    var gearboxTypes = ["manualis", "automata"];
+    for (var i = 0; i < 50; i++) {
+        cars = [
+            ...cars,
+            {
+                plateNumber: faker.vehicle.vin().substr(0, 6),
+                make: faker.vehicle.manufacturer(),
+                model: faker.vehicle.model(),
+                productionYear: faker.date.past().getYear(),
+                seatNumber: seatNumbers[~~(Math.random() * seatNumbers.length)],
+                mileage: faker.random.number(),
+                color: faker.vehicle.color(),
+                fuelType: faker.vehicle.fuel(),
+                gearboxType:
+                    gearboxTypes[~~(Math.random() * gearboxTypes.length)],
+                imgURL: faker.image.imageUrl(),
+            },
+        ];
+    }
+
+    CarModel.insertMany(cars, (error, docs) => {
+        console.log(error, docs);
+        mongoose.disconnect();
+    });
+});
