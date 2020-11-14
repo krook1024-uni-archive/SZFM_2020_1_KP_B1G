b9e77a1cadd9a2dac1ff31ba8203f00b072ce708
Tóth Csaba <tocsamanatee@gmail.com>
2020-11-14 18:45:20 +0100

    Implemented the User router. Closes #102

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,19 @@
 const express = require("express"),
-    router = express.Router();
+  router = express.Router();
+
+const userController = require("../controllers/userController")();
 
 router.get("/", (req, res) => {
-    res.json({ string: "hello world" });
+  const users = userController.findAll().then((users) => {
+    res.json(users);
+  });
+});
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  const user = userController.findOne(id).then((user) => {
+    res.json(user);
+  });
 });
 
 module.exports = router;
