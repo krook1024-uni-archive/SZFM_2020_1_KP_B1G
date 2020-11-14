2352346971725bcca95a73810fc8698be5f5c278
Molnár Antal <krook1024@gmail.com>
2020-11-14 20:26:51 +0100

    Update .eslintrc.js to match older eslint version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,11 @@
 module.exports = {
     "env": {
         "browser": false,
-        "es2021": false,
         "node": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
-        "ecmaVersion": 12
+        "ecmaVersion": 11
     },
     "rules": {
     }
