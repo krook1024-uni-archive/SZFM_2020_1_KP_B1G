ed37d9efa1fba6d6c675ca63a9f846d6afff6c5c
Molnár Antal <krook1024@gmail.com>
2020-11-14 11:56:26 +0100

    Added schemas for mongoose

---
+++ schemas/Car.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+
+const Car = new mongoose.Schema({
+    plateNumber: {
+        type: String,
+        required: true,
+    },
+    make: {
+        type: String,
+        required: true,
+    },
+    model: {
+        type: String,
+        required: true,
+    },
+    productionYear: {
+        type: Date,
+        required: true,
+    },
+    seatNumber: {
+        type: Integer,
+        required: true,
+    },
+    mileage: {
+        type: Integer,
+        required: true,
+    },
+    color: {
+        type: String,
+        required: true,
+    },
+    fuelType: {
+        type: String,
+        required: true,
+    },
+    gearboxType: {
+        type: String,
+        required: true,
+    },
+    imgURL: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    updatedAt: {
+        type: Date,
+        required: false,
+    },
+    deletedAt: {
+        type: Date,
+        required: false,
+    },
+});
+
+module.exports = Car;
---
+++ schemas/User.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const User = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true,
+    },
+    licenseCategory: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    updatedAt: {
+        type: Date,
+        required: false,
+    },
+    deletedAt: {
+        type: Date,
+        required: false,
+    },
+});
+
+module.exports = User;
