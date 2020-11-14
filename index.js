f4fa564791cf4339e776d4b187f8880310825e66
Molnár Antal <krook1024@gmail.com>
2020-11-14 13:11:40 +0100

    Restructure server

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const mongoose = require("mongoose");
+
+require("dotenv").config();
 
 const port = process.env.PORT || 3004;
 
-const indexRouter = require("./routes/index");
-const carsRouter = require("./routes/cars");
-const usersRouter = require("./routes/users");
-const authRouter = require("./routes/auth");
+console.log(`mongo_uri: ${process.env.MONGO_URI}`);
 
-app.use(morgan("tiny"));
+const mongo_config = {
+    auth: { authSource: "admin" },
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+mongoose
+    .connect(process.env.MONGO_URI, mongo_config)
+    .then(() => {
+        console.log("MongoDB connected");
 
-app.get("/", (req, res) => {
-    res.json({ string: "hello world" });
-});
+        const indexRouter = require("./routes/index");
+        const carsRouter = require("./routes/cars");
+        const usersRouter = require("./routes/users");
+        const authRouter = require("./routes/auth");
 
-app.use("/", indexRouter);
-app.use('/cars', carsRouter);
-app.use('/users', usersRouter);
-app.use('/auth', authRouter);
+        app.use(morgan("tiny"));
 
-app.listen(port, () => {});
+        app.get("/", (req, res) => {
+            res.json({ string: "hello world" });
+        });
+
+        app.use("/", indexRouter);
+        app.use("/cars", carsRouter);
+        app.use("/users", usersRouter);
+        app.use("/auth", authRouter);
+
+        app.listen(port, () => {
+            console.log(`Listening on port: ${port}`);
+        });
+    })
+    .catch((err) => console.log(err));
 
 module.exports = app;
