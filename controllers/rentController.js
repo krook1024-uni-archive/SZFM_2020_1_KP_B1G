ba2d5e035b3ec5eaf87a37e957b929cef780a5c0
Tóth Csaba <tocsamanatee@gmail.com>
2020-11-15 12:56:32 +0100

    Started working on the rentController. Closes #104

---
+++ controllers/rentController.js
@@ -0,0 +1,33 @@
+const model = require("../models/Rent");
+
+const rentController = () => {
+  const insertOne = (rent) => {
+    // TODO
+  };
+
+  const findOne = (id) => {
+    // TODO
+  };
+
+  const findAll = () => {
+    // TODO
+  };
+
+  const updateOne = (rent, id) => {
+    // TODO
+  };
+
+  const deleteOne = (id) => {
+    // TODO
+  };
+
+  return {
+    insertOne,
+    findOne,
+    findAll,
+    updateOne,
+    deleteOne,
+  };
+};
+
+module.exports = rentController;
