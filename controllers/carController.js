363731a5207940ecf451546dc271440e489e900c
Tóth Csaba <tocsamanatee@gmail.com>
2020-11-15 12:04:56 +0100

    Added Error handling to the carController. Closes #102

--- controllers/carController.js
+++ controllers/carController.js
@@ -3,57 +3,72 @@ const model = require("../models/Car");
 
 const carController = () => {
   const insertOne = async (car) => {
-    console.log("New Car Added", car);
-    const newCar = new Car(car);
-    await newCar.save();
+    try {
+      console.log("New Car Added", car);
+      const newCar = new Car(car);
+      await newCar.save();
 
-    return newCar;
+      return newCar;
+    } catch (error) {
+      console.log("Car validation failed.");
+    }
   };
 
   const findOne = async (id) => {
-    const car = await model.findById(id).exec();
+    try {
+      const car = await model.findById(id).exec();
 
-    return car;
+      return car;
+    } catch (error) {
+      console.log(`Car with id: ${id} not found`);
+    }
   };
 
   const findAll = async () => {
-    // todo: mongodbbol kikeresni
-    const cars = await model.find().exec();
-
-    return cars;
+    try {
+      const cars = await model.find().exec();
+      return cars;
+    } catch (error) {
+      console.log("No cars found.");
+    }
   };
 
   const updateOne = async (id, car) => {
-    const value = new Car(car);
-    const item = await model.findOne({ _id: id });
-    if (!item) console.log("car not found");
-    const updatedCar = await model.updateOne(
-      {
-        _id: id,
-      },
-      {
-        $set: {
-          plateNumber: value.plateNumber,
-          make: value.make,
-          model: value.model,
-          productionYear: value.productionYear,
-          seatNumber: value.seatNumber,
-          mileage: value.mileage,
-          color: value.color,
-          fuelType: value.fuelType,
-          gearboxType: value.gearboxType,
-          imgURL: value.imgURL,
+    try {
+      const value = new Car(car);
+      const updatedCar = await model.updateOne(
+        {
+          _id: id,
         },
-      }
-    );
+        {
+          $set: {
+            plateNumber: value.plateNumber,
+            make: value.make,
+            model: value.model,
+            productionYear: value.productionYear,
+            seatNumber: value.seatNumber,
+            mileage: value.mileage,
+            color: value.color,
+            fuelType: value.fuelType,
+            gearboxType: value.gearboxType,
+            imgURL: value.imgURL,
+          },
+        }
+      );
 
-    return updatedCar;
+      return updatedCar;
+    } catch (error) {
+      console.log(`Update error: Car with id: ${id} not found`);
+    }
   };
 
-
   const deleteOne = async (id) => {
-    const deletedCar = await model.deleteOne({ _id: id });
-    return deletedCar;
+    try {
+      const deletedCar = await model.deleteOne({ _id: id });
+      return deletedCar;
+    } catch (error) {
+      console.log(`Delete error: Car with id: ${id} not found`);
+    }
   };
 
   return {
@@ -61,7 +76,7 @@ const carController = () => {
     findOne,
     findAll,
     updateOne,
-    deleteOne
+    deleteOne,
   };
 };
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -13,53 +13,53 @@ const userController = () => {
   };
 
   const findOne = async (id) => {
-    try{
-    const user = await model.findById(id).exec();
-    return user;
-    } catch(error){
-      console.log(`User with id: ${id} not found`)
+    try {
+      const user = await model.findById(id).exec();
+      return user;
+    } catch (error) {
+      console.log(`User with id: ${id} not found`);
     }
   };
 
   const findAll = async () => {
-    try{
-    const users = await model.find().exec();
-    return users;
-    } catch(error){
-      console.log("No users found")
+    try {
+      const users = await model.find().exec();
+      return users;
+    } catch (error) {
+      console.log("No users found");
     }
   };
 
   const updateOne = async (id, user) => {
-    try{
-    const value = new User(user);
-    const updatedUser = await model.updateOne(
-      {
-        _id: id,
-      },
-      {
-        $set: {
-          name: value.name,
-          email: value.email,
-          password: value.password,
-          dateOfBirth: value.dateOfBirth,
-          licenseCategory: value.licenseCategory,
+    try {
+      const value = new User(user);
+      const updatedUser = await model.updateOne(
+        {
+          _id: id,
         },
-      }
-    );
+        {
+          $set: {
+            name: value.name,
+            email: value.email,
+            password: value.password,
+            dateOfBirth: value.dateOfBirth,
+            licenseCategory: value.licenseCategory,
+          },
+        }
+      );
 
-    return updatedUser;
-    } catch(error){
-      console.log(`Update error: User with id: ${id} not found`)
+      return updatedUser;
+    } catch (error) {
+      console.log(`Update error: User with id: ${id} not found`);
     }
   };
 
   const deleteOne = async (id) => {
-    try{
-    const deletedUser = await model.deleteOne({ _id: id });
-    return deletedUser;
-    } catch(error){
-      console.log(`Delete error: User with id: ${id} not found`)
+    try {
+      const deletedUser = await model.deleteOne({ _id: id });
+      return deletedUser;
+    } catch (error) {
+      console.log(`Delete error: User with id: ${id} not found`);
     }
   };
 
