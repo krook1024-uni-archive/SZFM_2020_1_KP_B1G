8cfd3e9084423fd48c4799bd15a3a2964d08a07c
Tóth Csaba <tocsamanatee@gmail.com>
2020-11-15 13:49:56 +0100

    Implemented the insertOne and findOne methods in rentController. Closes #104

--- controllers/rentController.js
+++ controllers/rentController.js
@@ -1,12 +1,26 @@
+const Rent = require("../models/Rent");
 const model = require("../models/Rent");
 
 const rentController = () => {
-  const insertOne = (rent) => {
-    // TODO
+  const insertOne = async (rent) => {
+    try {
+      console.log("New rent added", rent);
+      const newRent = new Rent(rent);
+      await newRent.save();
+
+      return newRent;
+    } catch (error) {
+      console.log("Rent validation failed.");
+    }
   };
 
-  const findOne = (id) => {
-    // TODO
+  const findOne = async (id) => {
+    try {
+      const rent = await model.findById(id).exec();
+      return rent;
+    } catch (error) {
+      console.log(`Rent with id: ${id} not found`);
+    }
   };
 
   const findAll = async () => {
