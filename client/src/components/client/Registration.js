fd45b3c30e510fac3b78e68ab44a2e9fc8493cb3
Czinke Márton <czinke.marci@gmail.com>
2020-11-25 13:27:35 +0100

    Implement form validation methods

--- client/src/components/client/Registration.js
+++ client/src/components/client/Registration.js
@@ -1,87 +1,191 @@
-import React, { useState } from "react";
-import Modal from "react-bootstrap/Modal";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+import React, { useState, useRef } from "react";
 import Col from "react-bootstrap/Col";
+import { Modal, Button, Form, Nav } from "react-bootstrap";
 
 const Registration = () => {
-  const firstName = useRef();
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   const lastName = useRef();
+  const firstName = useRef();
   const email = useRef();
-  const password = useRef();
+  const pwd = useRef();
   const licenseType = useRef();
   const licenseDate = useRef();
+
+  const validateForm = () => {
+    if (validateName(lastName.current.value, firstName.current.value)) {
+      console.log(
+        "Név: " + lastName.current.value + " " + firstName.current.value
+      );
+    }
+    if (validateEmail(email.current.value)) {
+      console.log("Email: " + email.current.value);
+    }
+    if (validatePassword(pwd.current.value)) {
+      console.log("Pwd: " + pwd.current.value);
+    }
+    if (validateLicenseType(licenseType.current.value)) {
+      console.log("License Type: " + licenseType.current.value);
+    }
+    if (validateLicenseDate(licenseDate.current.value)) {
+      console.log("License Date: " + licenseDate.current.value);
+    }
+  };
+  const validateName = (lastName, firstName) => {
+    if (lastName !== "" && lastName != null) {
+      if (firstName !== "" && firstName != null) return true;
+      else return false;
+    } else return false;
+  };
+  const validateEmail = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email));
+  };
+  const validatePassword = (password) => {
+    if (password.length >= 5) return true;
+    else return false;
+  };
+  const validateLicenseType = (type) => {
+    if (type === "-") return false;
+    else return true;
+  };
+  const validateLicenseDate = (date) => {
+    var licenseDates = date.split("-");
+    var nowDates = getCurrentDate().split("-");
+    var licenseYear = licenseDates[0];
+    var licenseMonth = licenseDates[1];
+    var licenseDay = licenseDates[2];
+    var nowYear = nowDates[0];
+    var nowMonth = nowDates[1];
+    var nowDay = nowDates[2];
+    if (
+      licenseYear <= nowYear &&
+      licenseMonth <= nowMonth &&
+      licenseDay <= nowDay
+    )
+      return true;
+    else return false;
+  };
+
+  function getCurrentDate(separator = "-") {
+    let newDate = new Date();
+    let date = newDate.getDate();
+    let month = newDate.getMonth() + 1;
+    let year = newDate.getFullYear();
+
+    var now = `${year}${separator}${
+      month < 10 ? `0${month}` : `${month}`
+    }${separator}${date}`;
+    return now;
+  }
+
   return (
-    <Modal.Dialog>
-      <Modal.Header closeButton>
-        <Modal.Title>Regisztráció</Modal.Title>
-      </Modal.Header>
+    <>
+      <Nav.Item variant="primary">
+        <Nav.Link onClick={handleShow}>Registration</Nav.Link>
+      </Nav.Item>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Dialog>
+          <Modal.Header closeButton>
+            <Modal.Title>Regisztráció</Modal.Title>
+          </Modal.Header>
 
-      <Modal.Body>
-        <Form>
-          <Form.Group controlId="formName">
-            <Form.Label>Név</Form.Label>
-            <Form.Row>
-              <Col>
-                <Form.Control placeholder="Vezetéknév" ref={lastName} />
-              </Col>
-              <Col>
-                <Form.Control placeholder="Keresztnév" ref={firstName} />
-              </Col>
-            </Form.Row>
-          </Form.Group>
-          <Form.Group controlId="formEmail">
-            <Form.Label>Email cím</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Enter email.."
-              ref={email}
-            />
-            <Form.Text className="text-muted">pl: pelda@email.com</Form.Text>
-          </Form.Group>
+          <Modal.Body>
+            <Form>
+              <Form.Group controlId="formName">
+                <Form.Label>Név</Form.Label>
+                <Form.Row>
+                  <Col>
+                    <Form.Control
+                      placeholder="Vezetéknév"
+                      name="lastName"
+                      ref={lastName}
+                    />
+                  </Col>
+                  <Col>
+                    <Form.Control
+                      placeholder="Keresztnév"
+                      name="firstName"
+                      ref={firstName}
+                    />
+                  </Col>
+                </Form.Row>
+              </Form.Group>
+              <Form.Group controlId="formEmail">
+                <Form.Label>Email cím</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email.."
+                  name="email"
+                  ref={email}
+                />
+                <Form.Text className="text-muted">
+                  pl: pelda@email.com
+                </Form.Text>
+              </Form.Group>
 
-          <Form.Group controlId="formPassword">
-            <Form.Label>Jelszó</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              id="inputPassword"
-              ref={password}
-            />
-          </Form.Group>
-          <Form.Group controlId="formLicenseType">
-            <Form.Label>Jogosítvány típusa</Form.Label>
-            <Form.Control as="select" id="inputLiceseType" ref={licenseType}>
-              <option>AM</option>
-              <option>A1</option>
-              <option>A2</option>
-              <option>A</option>
-              <option>B1</option>
-              <option>B</option>
-              <option>B+E</option>
-              <option>C1</option>
-              <option>C1+E</option>
-              <option>C</option>
-              <option>C+E</option>
-              <option>D1</option>
-              <option>D1+E</option>
-              <option>D</option>
-              <option>D+E</option>
-            </Form.Control>
-          </Form.Group>
-          <Form.Group controlId="formLicenseDate">
-            <Form.Label>Jogosítvány megszerzés dátuma</Form.Label>
-            <br></br>
-            <input type="date" id="inputLicensedate" ref={licenseDate}></input>
-          </Form.Group>
-        </Form>
-      </Modal.Body>
+              <Form.Group controlId="formPassword">
+                <Form.Label>Jelszó</Form.Label>
+                <Form.Control
+                  name="pwd"
+                  type="password"
+                  placeholder="Password"
+                  id="inputPassword"
+                  ref={pwd}
+                />
+              </Form.Group>
+              <Form.Group controlId="formLicenseType">
+                <Form.Label>Jogosítvány típusa</Form.Label>
+                <Form.Control
+                  as="select"
+                  id="inputLiceseType"
+                  name="licenseType"
+                  ref={licenseType}
+                >
+                  <option>-</option>
+                  <option>AM</option>
+                  <option>A1</option>
+                  <option>A2</option>
+                  <option>A</option>
+                  <option>B1</option>
+                  <option>B</option>
+                  <option>B+E</option>
+                  <option>C1</option>
+                  <option>C1+E</option>
+                  <option>C</option>
+                  <option>C+E</option>
+                  <option>D1</option>
+                  <option>D1+E</option>
+                  <option>D</option>
+                  <option>D+E</option>
+                </Form.Control>
+              </Form.Group>
+              <Form.Group controlId="formLicenseDate">
+                <Form.Label>Jogosítvány megszerzés dátuma</Form.Label>
+                <br></br>
+                <input
+                  type="date"
+                  id="inputLicensedate"
+                  name="licenseDate"
+                  ref={licenseDate}
+                ></input>
+              </Form.Group>
+            </Form>
+          </Modal.Body>
 
-      <Modal.Footer>
-        <Button variant="secondary">Close</Button>
-        <Button variant="primary">Regisztráció</Button>
-      </Modal.Footer>
-    </Modal.Dialog>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={validateForm}>
+              Regisztráció
+            </Button>
+          </Modal.Footer>
+        </Modal.Dialog>
+      </Modal>
+    </>
   );
 };
 export default Registration;
