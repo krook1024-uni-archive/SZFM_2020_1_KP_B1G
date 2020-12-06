9f66e6a6b4d81128ac94dc3b0d6455c3a7deb2a3
fulekylaszlo <fulekylaszlo@gmail.com>
2020-12-06 13:28:04 +0100

    Created ProfilePage for the website.

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,14 +1,30 @@
 import React from "react";
 import NavBar from "../components/client/NavBar";
-import { Container } from "react-bootstrap";
 
-const Profile = () => {
-  return (
-    <>
-      <NavBar />
-      <Container>I'm the profile page component.</Container>
-    </>
-  );
-};
 
-export default Profile;
+
+export default class Profile extends React.Component {
+   state = {
+      loading: true
+}
+
+   async componentDidMount() {
+   const url = "localhost:3000/";
+   const response = await fetch (url);
+   const data = await response.json();
+   console.log(data);
+}
+
+render() {
+   return (
+   <>
+   <NavBar/>
+      <div>
+      {this.state.loading ? <div>loading...</div> : <div> person..</div>}
+      </div>
+   </>
+);
+
+
+}
+}
